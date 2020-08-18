db6f133263af1aa12be441c5ea4da4511a363fc3
belluominus <jpscbelluominus@gmail.com>
2020-08-18 12:31:27 -0300

    First Commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,15 +4,16 @@ const { uuid } = require('uuidv4');
 const app = express();
 app.use(express.json());
 
-const Projects = [];
+const projects = [];
 
 app.get('/projects', (request, response) =>{
-  // const {title, owner} = request.query;
+  const {title} = request.query;
 
-  // console.log(title);
-  // console.log(owner);
-  
-  return response.json(Projects);
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+  return response.json(results);
 });
 
 app.post('/projects', (request, response) =>{
@@ -20,30 +21,43 @@ app.post('/projects', (request, response) =>{
 
   const project = { id: uuid(), title, owner}
   
-  Projects.push(project)
+  projects.push(project)
 
   return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) =>{
   const {id} = request.params;
+  const {title, owner} = request.body;
 
   const projectIndex = projects.findIndex(project => project.id === id); 
 
   if (projectIndex < 0) {
-    return response.json({error: "Project not fund"});
+    return response.status(400).json({error: 'Project not found'});
+  }
+  const project = {
+    id,
+    title,
+    owner,
   };
-  return response.json({ok: "OK"});
+  
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) =>{
   const {id} = request.params;
 
-  console.log(id);
-  return response.json([
-    'projeto 2',
-    'projeto 3'
-  ]);
+  const projectIndex = projects.findIndex(project => project.id === id); 
+
+  if (projectIndex < 0) {
+    return response.status(400).json({error: 'Project not found'});
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () =>{
